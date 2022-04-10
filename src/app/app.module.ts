43f37023b7d246781a83479cd080f1d0f64fbe26
Samuel Martins <samuel.martins4.sm@gmail.com>
2022-04-10 16:04:34 +0300

    feature: add a quotes form component for publishing new quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 
 @NgModule({
-  declarations: [AppComponent, QuotesComponent, QuoteDetailsComponent],
+  declarations: [AppComponent, QuotesComponent, QuoteDetailsComponent, QuotesFormComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
